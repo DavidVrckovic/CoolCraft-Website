0ccd53c9f45c4cb6e30ad8d4a353a728f8c5b95c
David <84281374+DavidVrckovic@users.noreply.github.com>
2023-05-25 23:08:09 +0200

    Improve the authentication scripts (#3)
    * Update php/links.php
    
    Check if the session is not active and cookies are set, then update the session with cookie data.
    
    * Update php/mysql_connection.php
    
    Now using a multidimensional array to pass values from ini file.
    
    * Update php/auth.php
    
    Not using nested if statements anymore.
    
    * Update login/index.php
    
    * Update logout/index.php

--- logout/index.php
+++ logout/index.php
@@ -3,12 +3,32 @@
 session_start();
 session_destroy();
 
-// Check if the cookie is set
-if (isset($_COOKIE['login'])) {
-    unset($_COOKIE['login']);
-    setcookie("login", "", time()-3600, "/");
+// Check if there is local data (cookie)
+if (isset($_COOKIE["loggedin"])) {
+
+    // Unset local data (cookies)
+    unset($_COOKIE["loggedin"]);
+    setcookie("loggedin", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_id"]);
+    setcookie("user_id", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_username"]);
+    setcookie("user_username", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_email"]);
+    setcookie("user_email", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_lastlogin"]);
+    setcookie("user_lastlogin", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_regdate"]);
+    setcookie("user_regdate", "", time() - 3600, "/");
+
+    unset($_COOKIE["user_password"]);
+    setcookie("user_password", "", time() - 3600, "/");
 }
 
-// Redirect to the index page
+// Redirect a user to the index page
 header("Location: ../");
 ?>
